e2c06ab1ad5c0e63ba0085c4d0ec620ef68657ff
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-01 23:25:11 -0400

    progress

--- sshrd.sh
+++ sshrd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-chmod +x macos/*
+
+: ${1?"Usage: $0 ipsw link"}
+: ${2?"Usage: $0 board cfg (no AP part)"}
+
+
 macos/pzb -g BuildManifest.plist $1
+macos/pzb -g Firmware/dfu/iBSS.$2.RELEASE.im4p $1
