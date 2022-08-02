ef5a1f30243b1e406562bc26dca400d7bba03d09
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-02 00:55:11 -0400

    Update sshrd.sh

--- sshrd.sh
+++ sshrd.sh
@@ -7,5 +7,5 @@ chmod +x macos/*
 : ${2?"Usage: $0 board cfg, can be iphone6 for example too (no AP part)"}
 
 
-macos/pzb -g BuildManifest.plist $1
-macos/pzb -g Firmware/dfu/iBSS.$2.RELEASE.im4p $1
+macos/pzb -g BuildManifest.plist $1 1> /dev/null
+macos/pzb -g Firmware/dfu/iBSS.$2.RELEASE.im4p $1 1> /dev/null
