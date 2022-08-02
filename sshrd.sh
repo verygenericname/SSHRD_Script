57f3f5fffd3f2250df4027ec8abd0b17d5c4cfe1
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-02 01:16:53 -0400

    nvm

--- sshrd.sh
+++ sshrd.sh
@@ -5,8 +5,6 @@ chmod +x macos/*
 
 : ${1?"1st argument: ipsw link"}
 : ${2?"2nd argument: board cfg, can be iphone6 for example too (no AP part, lowercase)"}
-: ${3?"3rd argument: model identifier (case-sensitive)"}
-: ${4?"4th argument: ios version from ipsw link"}
 
 
 macos/pzb -g BuildManifest.plist $1 1> /dev/null
