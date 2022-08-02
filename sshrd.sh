05cb5e00c72cd45fb73ff9b1a86f1dbfa8841cf5
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-02 01:14:08 -0400

    Update sshrd.sh

--- sshrd.sh
+++ sshrd.sh
@@ -4,8 +4,8 @@ chmod +x macos/*
 
 
 : ${1?"1st argument: ipsw link"}
-: ${2?"2nd argument: board cfg, can be iphone6 for example too (no AP part)"}
-: ${3?"3rd argument: model identifier"}
+: ${2?"2nd argument: board cfg, can be iphone6 for example too (no AP part, lowercase)"}
+: ${3?"3rd argument: model identifier (case-sensitive)"}
 : ${4?"4th argument: ios version from ipsw link"}
 
 
