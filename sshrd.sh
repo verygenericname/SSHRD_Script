ce12d99ff9801947e74785cdaee46168c8d24793
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-02 21:59:03 -0400

    Update sshrd.sh

--- sshrd.sh
+++ sshrd.sh
@@ -9,9 +9,12 @@ chmod +x macos/*
 : ${4?"4th argument: iv and key combined together for the ibss from the ipsw link you provided. You can get them from the iphonewiki"}
 : ${5?"5th argument: iv and key combined together for the ibec from the ipsw link you provided. You can get them from the iphonewiki"}
 : ${6?"6th argument: due to the fact i have no idea how to parse a BuildManifest in a shell script, please provide the filename of the restore ramdisk dmg here"}
-
+: ${7?"7th argument: here too, i have no idea how to parse the BuildManifest, please provide kernelcache filename"}
 
 macos/pzb -g BuildManifest.plist $1 1> /dev/null
 macos/pzb -g Firmware/dfu/iBSS.$2.RELEASE.im4p $1
 macos/pzb -g Firmware/dfu/iBEC.$2.RELEASE.im4p $1
+macos/pzb -g Firmware/all_flash/DeviceTree.$2ap.im4p
+macos/pzb -g $6.trustcache $1
+macos/pzb -g $7 $1
 macos/pzb -g $6 $1
