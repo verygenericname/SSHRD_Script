5655d2e94fa8fe287f26c783fb4466993f954e27
Nathan Senter <nathan@Nathan-PC.home>
2022-08-04 18:27:56 -0400

    add some more

--- sshrd.sh
+++ sshrd.sh
@@ -11,6 +11,7 @@ chmod +x macos/*
 : ${6?"6th argument: due to the fact i have no idea how to parse a BuildManifest in a shell script, please provide the filename of the restore ramdisk dmg here"}
 : ${7?"7th argument: here too, i have no idea how to parse the BuildManifest, please provide kernelcache filename"}
 
+img4tool -e -s $3 -m IM4M
 macos/pzb -g BuildManifest.plist $1 1> /dev/null
 macos/pzb -g Firmware/dfu/iBSS.$2.RELEASE.im4p $1
 macos/pzb -g Firmware/dfu/iBEC.$2.RELEASE.im4p $1
@@ -18,3 +19,5 @@ macos/pzb -g Firmware/all_flash/DeviceTree.$2ap.im4p $1
 macos/pzb -g Firmware/$6.trustcache $1
 macos/pzb -g $7 $1
 macos/pzb -g $6 $1
+img4 -i iBSS.$2.RELEASE.im4p -o iBSS.dec -k $4
+img4 -i iBEC.$2.RELEASE.im4p -o iBEC.dec -k $5
