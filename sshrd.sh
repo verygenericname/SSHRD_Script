6feec3a29644e6521e22316b4bb6bdcc39b265a5
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-04 19:58:40 -0400

    Add files via upload

--- sshrd.sh
+++ sshrd.sh
@@ -31,3 +31,47 @@ python3 kerneldiff.py kcache.raw kcache.patched
 macos/img4 -i $7 -o kernelcache.img4 -M IM4M -T rkrn -P kc.bpatch
 macos/img4 -i DeviceTree.$2ap.im4p -o devicetree.img4 -M IM4M -T rdtr
 macos/img4 -i $6.trustcache -o trustcache.img4 -M IM4M -T rtsc
+macos/img4 -i $6 -o ramdisk.dmg
+hdiutil resize -size 120MB ramdisk.dmg
+hdiutil attach -mountpoint /tmp/SSHRD ramdisk.dmg
+macos/gtar -x --no-overwrite-dir -f ssh.tar -C /tmp/SSHRD/
+macos/ldid -Sdd_ent.xml /tmp/SSHRD/bin/dd
+macos/ldid -Sent.xml /tmp/SSHRD/sbin/mount
+macos/ldid -M -Sent.xml /tmp/SSHRD/bin/*
+macos/ldid -M -Sent.xml /tmp/SSHRD/usr/bin/*
+macos/ldid -M -Sent.xml /tmp/SSHRD/usr/sbin/*
+macos/ldid -M -Sent.xml /tmp/SSHRD/usr/local/bin/*
+macos/ldid -M -Sent.xml /tmp/SSHRD/System/Library/Filesystems/apfs.fs/apfs*
+macos/ldid -M -Sent.xml /tmp/SSHRD/System/Library/Filesystems/apfs.fs/mount_apfs
+macos/ldid -M -Sent.xml /tmp/SSHRD/System/Library/Filesystems/apfs.fs/slurpAPFSMeta
+macos/ldid -M -Sent.xml /tmp/SSHRD/System/Library/Filesystems/apfs.fs/newfs_apfs
+macos/ldid -M -Sent.xml /tmp/SSHRD/System/Library/Filesystems/apfs.fs/fsck_apfs
+macos/ldid -M -Sent.xml /tmp/SSHRD/System/Library/Filesystems/apfs.fs/hfs_convert
+hdiutil detach -force /tmp/SSHRD
+hdiutil resize -sectors min ramdisk.dmg
+macos/img4 -i ramdisk.dmg -o ramdisk.img4 -M IM4M -A -T rdsk
+mv ramdisk.img4 sshramdisk
+mv trustcache.img4 sshramdisk
+mv devicetree.img4 sshramdisk
+mv kernelcache.img4 sshramdisk
+mv iBEC.img4 sshramdisk
+mv iBSS.img4 sshramdisk
+echo "we are done, please use boot.sh to boot your device"
+echo cleanup...
+rm iBSS.$2.RELEASE.im4p
+rm iBEC.$2.RELEASE.im4p 
+rm iBSS.dec
+rm iBEC.dec
+rm iBSS.patched
+rm iBEC.patched
+rm $7
+rm $6
+rm $6.trustcache
+rm BuildManifest.plist
+rm kcache.raw
+rm kcache.patched
+rm DeviceTree.$2ap.im4p
+rm IM4M
+rm kc.bpatch
+touch kc.bpatch
+rm ramdisk.dmg
