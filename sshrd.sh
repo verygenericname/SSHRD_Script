327859eee014f6cc22838a38e41f7c59c7218492
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-04 22:48:49 -0400

    Update sshrd.sh

--- sshrd.sh
+++ sshrd.sh
@@ -4,7 +4,7 @@ chmod +x macos/*
 
 
 : ${1?"1st argument: ipsw link"}
-: ${2?"2nd argument: board cfg, can be iphone6 for example too (no AP part, lowercase)"}
+: ${2?"2nd argument: board cfg (no AP part, lowercase)"}
 : ${3?"3rd argument: can be any shsh blob, just make sure it's from the same ecid as your phone"}
 : ${4?"4th argument: iv and key combined together for the ibss from the ipsw link you provided. You can get them from the iphonewiki"}
 : ${5?"5th argument: iv and key combined together for the ibec from the ipsw link you provided. You can get them from the iphonewiki"}
