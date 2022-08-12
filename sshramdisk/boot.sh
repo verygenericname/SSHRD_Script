db04a0689c769e5eb31ceb55e7ac6bed293fe96c
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-12 16:20:41 -0400

    add a reset option

--- sshramdisk/boot.sh
+++ sshramdisk/boot.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+if [[ "$1" == 'reset' ]]; then
+irecovery -f iBSS.img4
+irecovery -f iBSS.img4
+sleep 2
+irecovery -f iBEC.img4
+sleep 2
+irecovery -c "setenv oblit-inprogress 5"
+irecovery -c saveenv
+irecovery -c reset
+fi
+
 irecovery -f iBSS.img4
 irecovery -f iBSS.img4
 sleep 2
