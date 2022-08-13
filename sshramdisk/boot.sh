dae2c353827501efb0b34291c36c59de2a38dbe1
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-08-13 14:31:03 -0400

    nonce setter and improvements

--- sshramdisk/boot.sh
+++ sshramdisk/boot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$1" == 'reset' ]]; then
 irecovery -f iBSS.img4
+set -e
 irecovery -f iBSS.img4
 sleep 2
 irecovery -f iBEC.img4
@@ -8,10 +9,28 @@ sleep 2
 irecovery -c "setenv oblit-inprogress 5"
 irecovery -c saveenv
 irecovery -c reset
+echo "device should now show a progress bar when booting and then go to setup screen"
+exit
+fi
+
+if [[ "$1" == 'set-nonce' ]]; then
+: ${2?"2nd argument: generator here"}
+
+irecovery -f iBSS.img4
+set -e
+irecovery -f iBSS.img4
+sleep 2
+irecovery -f iBEC.img4
+sleep 2
+irecovery -c "setenv com.apple.System.boot-nonce $2"
+irecovery -c saveenv
+irecovery -c reset
+echo "nonce set to $2 successfully"
 exit
 fi
 
 irecovery -f iBSS.img4
+set -e
 irecovery -f iBSS.img4
 sleep 2
 irecovery -f iBEC.img4
@@ -24,3 +43,4 @@ irecovery -f trustcache.img4
 irecovery -c firmware
 irecovery -f kernelcache.img4
 irecovery -c bootx
+echo "device should show text on screen now."
