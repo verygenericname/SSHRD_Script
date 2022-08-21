e99f8bd26227a16681ddfc0ed3cdaac9bc38d59f
verygenericname <dedbeddedbed@gmail.com>
2022-08-21 14:59:41 -0400

    huge overhall, ipad fixed.

--- sshrd.sh
+++ sshrd.sh
@@ -137,112 +137,36 @@ $oscheck/gaster pwn
 if [[ "$2" == "" ]]; then
 $oscheck/img4tool -e -s shsh/${check}.shsh -m work/IM4M
 else
-$oscheck/img4tool -e -s "$3" -m work/IM4M
+$oscheck/img4tool -e -s "$2" -m work/IM4M
 fi
 cd work
 ../$oscheck/pzb -g BuildManifest.plist $1
-if [[ "$4" == "" ]]; then
-    ../$oscheck/pzb -g Firmware/dfu/iBSS.$replace.RELEASE.im4p $1
-    ../$oscheck/pzb -g Firmware/dfu/iBEC.$replace.RELEASE.im4p $1
-else
-../$oscheck/pzb -g Firmware/dfu/iBSS.$4.RELEASE.im4p $1
-../$oscheck/pzb -g Firmware/dfu/iBEC.$4.RELEASE.im4p $1
-fi
-../$oscheck/pzb -g Firmware/all_flash/DeviceTree.${replace}ap.im4p $1
+../$oscheck/pzb -g $(awk "/${replace}/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) $1
+../$oscheck/pzb -g $(awk "/${replace}/{x=1}x&&/iBEC[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) $1
+../$oscheck/pzb -g $(awk "/${replace}/{x=1}x&&/DeviceTree[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) $1
 if [[ "$oscheck" == 'Darwin' ]]; then
 ../$oscheck/pzb -g Firmware/$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1).trustcache $1
 else
 ../$oscheck/pzb -g Firmware/$(../Linux/PlistBuddy BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g').trustcache $1
 fi
-if [[ "$replace" == "n66m" ]]; then
-../$oscheck/pzb -g kernelcache.release.n66 $1
-elif [[ "$replace" == "n71m" ]]; then
-../$oscheck/pzb -g kernelcache.release.n71 $1
-elif [[ "$replace" == "n69" ]] || [[ "$replace" == "n69u" ]]; then
-../$oscheck/pzb -g kernelcache.release.iphone8b $1
-elif [[ "$replace" == "d10" ]] || [[ "$replace" == "d11" ]]; then
-../$oscheck/pzb -g kernelcache.release.iphone9 $1
-elif [[ "$replace" == "d22" ]]; then
-../$oscheck/pzb -g kernelcache.release.iphone10b $1
-elif [[ "$replace" == "d20" ]] || [[ "$replace" == "d21" ]]; then
-../$oscheck/pzb -gkernelcache.release.iphone10 $1
-elif [[ "$replace" == "n61" ]] || [[ "$replace" == "n56" ]]; then
-../$oscheck/pzb -g kernelcache.release.iphone7 $1
-elif [[ "$replace" == "j132" ]] || [[ "$replace" == "j140a" ]] || [[ "$replace" == "j140k" ]] || [[ "$replace" == "j152f" ]] || [[ "$replace" == "j213" ]] || [[ "$replace" == "j214k" ]] || [[ "$replace" == "j215" ]] || [[ "$replace" == "j223" ]] || [[ "$replace" == "j230k" ]] || [[ "$replace" == "j680" ]] || [[ "$replace" == "j780" ]]; then
-../$oscheck/pzb -g kernelcache.release.ibridge2p $1
-elif [[ "$replace" == "j137" ]] || [[ "$replace" == "j160" ]] || [[ "$replace" == "j174" ]] || [[ "$replace" == "j185" ]] || [[ "$replace" == "j185f" ]]; then
-../$oscheck/pzb -g kernelcache.release.ibridge2d $1
-elif [[ "$4" == "" ]]; then
-../$oscheck/pzb -g kernelcache.release.$replace $1
-else
-../$oscheck/pzb -g kernelcache.release.$4 $1
-fi
+../$oscheck/pzb -g $(awk "/${replace}/{x=1}x&&/kernelcache.release/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) $1
 if [[ "$oscheck" == 'Darwin' ]]; then
 ../$oscheck/pzb -g $(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1) $1
 else
 ../$oscheck/pzb -g $(../Linux/PlistBuddy BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g') $1
 fi
 cd ..
-if [[ "$4" == "" ]]; then
-    $oscheck/gaster decrypt work/iBSS.$replace.RELEASE.im4p work/iBSS.dec
-    $oscheck/gaster decrypt work/iBEC.$replace.RELEASE.im4p work/iBEC.dec
-else
-    $oscheck/gaster decrypt work/iBSS.$4.RELEASE.im4p work/iBSS.dec
-    $oscheck/gaster decrypt work/iBEC.$4.RELEASE.im4p work/iBEC.dec
-fi
+$oscheck/gaster decrypt work/$(awk "/${replace}/{x=1}x&&/iBSS[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//') work/iBSS.dec
+$oscheck/gaster decrypt work/$(awk "/${replace}/{x=1}x&&/iBEC[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//') work/iBEC.dec
 $oscheck/iBoot64Patcher work/iBSS.dec work/iBSS.patched
 $oscheck/img4 -i work/iBSS.patched -o sshramdisk/iBSS.img4 -M work/IM4M -A -T ibss
 $oscheck/iBoot64Patcher work/iBEC.dec work/iBEC.patched -n -b "rd=md0 debug=0x2014e -v wdt=-9999999"
 $oscheck/img4 -i work/iBEC.patched -o sshramdisk/iBEC.img4 -M work/IM4M -A -T ibec
-if [[ "$replace" == "n66m" ]]; then
-$oscheck/img4 -i work/kernelcache.release.n66 -o work/kcache.raw
-elif [[ "$replace" == "n71m" ]]; then
-$oscheck/img4 -i work/kernelcache.release.n71 -o work/kcache.raw
-elif [[ "$replace" == "n69" ]] || [[ "$replace" == "n69u" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone8b -o work/kcache.raw
-elif [[ "$replace" == "d10" ]] || [[ "$replace" == "d11" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone9 -o work/kcache.raw
-elif [[ "$replace" == "d22" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone10b -o work/kcache.raw
-elif [[ "$replace" == "d20" ]] || [[ "$replace" == "d21" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone10 -o work/kcache.raw
-elif [[ "$replace" == "n61" ]] || [[ "$replace" == "n56" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone7 -o work/kcache.raw
-elif [[ "$replace" == "j132" ]] || [[ "$replace" == "j140a" ]] || [[ "$replace" == "j140k" ]] || [[ "$replace" == "j152f" ]] || [[ "$replace" == "j213" ]] || [[ "$replace" == "j214k" ]] || [[ "$replace" == "j215" ]] || [[ "$replace" == "j223" ]] || [[ "$replace" == "j230k" ]] || [[ "$replace" == "j680" ]] || [[ "$replace" == "j780" ]]; then
-$oscheck/img4 -i work/kernelcache.release.ibridge2p -o work/kcache.raw
-elif [[ "$replace" == "j137" ]] || [[ "$replace" == "j160" ]] || [[ "$replace" == "j174" ]] || [[ "$replace" == "j185" ]] || [[ "$replace" == "j185f" ]]; then
-$oscheck/img4 -i work/kernelcache.release.ibridge2d -o work/kcache.raw
-elif [[ "$4" == "" ]]; then
-$oscheck/img4 -i work/kernelcache.release.$replace -o work/kcache.raw
-else
-$oscheck/img4 -i work/kernelcache.release.$4 -o work/kcache.raw
-fi
+$oscheck/img4 -i work/$(awk "/${replace}/{x=1}x&&/kernelcache.release/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) -o work/kcache.raw
 $oscheck/Kernel64Patcher work/kcache.raw work/kcache.patched -a
 python3 kerneldiff.py work/kcache.raw work/kcache.patched work/kc.bpatch
-if [[ "$replace" == "n66m" ]]; then
-$oscheck/img4 -i work/kernelcache.release.n66 -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "n71m" ]]; then
-$oscheck/img4 -i work/kernelcache.release.n71 -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "n69" ]] || [[ "$replace" == "n69u" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone8b -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "d10" ]] || [[ "$replace" == "d11" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone9 -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "d22" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone10b -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "d20" ]] || [[ "$replace" == "d21" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone10 -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "n61" ]] || [[ "$replace" == "n56" ]]; then
-$oscheck/img4 -i work/kernelcache.release.iphone7 -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "j132" ]] || [[ "$replace" == "j140a" ]] || [[ "$replace" == "j140k" ]] || [[ "$replace" == "j152f" ]] || [[ "$replace" == "j213" ]] || [[ "$replace" == "j214k" ]] || [[ "$replace" == "j215" ]] || [[ "$replace" == "j223" ]] || [[ "$replace" == "j230k" ]] || [[ "$replace" == "j680" ]] || [[ "$replace" == "j780" ]]; then
-$oscheck/img4 -i work/kernelcache.release.ibridge2p -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$replace" == "j137" ]] || [[ "$replace" == "j160" ]] || [[ "$replace" == "j174" ]] || [[ "$replace" == "j185" ]] || [[ "$replace" == "j185f" ]]; then
-$oscheck/img4 -i work/kernelcache.release.ibridge2d -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-elif [[ "$4" == "" ]]; then
-$oscheck/img4 -i work/kernelcache.release.$replace -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-else
-$oscheck/img4 -i work/kernelcache.release.$4 -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
-fi
-$oscheck/img4 -i work/DeviceTree.${replace}ap.im4p -o sshramdisk/devicetree.img4 -M work/IM4M -T rdtr
+$oscheck/img4 -i work/$(awk "/${replace}/{x=1}x&&/kernelcache.release/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [[ "$oscheck" == 'Linux' ]]; then echo "-J"; fi`
+$oscheck/img4 -i work/$(awk "/${replace}/{x=1}x&&/DeviceTree[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]//') -o sshramdisk/devicetree.img4 -M work/IM4M -T rdtr
 if [[ "$oscheck" == 'Darwin' ]]; then
 $oscheck/img4 -i work/$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1).trustcache -o sshramdisk/trustcache.img4 -M work/IM4M -T rtsc
 $oscheck/img4 -i work/$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1) -o work/ramdisk.dmg
